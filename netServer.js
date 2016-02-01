f004827565ae11d3d07bd57c0cb68cfad6a40783
agentpietrucha <agentpietrucha@gmail.com>
2016-02-01 23:46:17 +0100

    added message when someone leaves the converstion

--- netServer.js
+++ netServer.js
@@ -1,24 +1,47 @@
 var net = require('net');
 
-
 var sockets = [];
+
 var server = net.createServer(function(socket){
+	var number = 0;
+	var x = 0;
+
 	sockets.push(socket);
+	var name = '';
 	console.log('number of sockets: ' + sockets.length);
 
+	function removeWhitespace(str){
+		return str.replace(/(\r\n|\n|\r)/gm,"");
+	}
 	socket.write('gimmie you nick nigga');
 
-
 	socket.on('data', function(chunk){
-		console.log('client' + sockets.indexOf(socket) + chunk.toString());
+		var data = removeWhitespace(chunk.toString());
+		if(number === 0){
+			socket.write('Hi, ' + data );
+			number += 1;
+			name += data;
+			/*if(data === name){
+				console.log('this name is in use, choose another name');
+			}*/
+		}
+		console.log('client:' + name + ' ' + data);
 		for(var i = 0; i < sockets.length; i++){
-			sockets[i].write(chunk.toString());
+			if(x === 0){
+			sockets[i].write(name + ' ' + 'joined');
+			x += 1;
+			} else{
+				sockets[i].write(name + ': ' + data);
+			}
 		}
 	})
 	socket.on('end', function(){
 		var index = sockets.indexOf(socket);
 		sockets.splice(index, 1);
-		console.log("KUNIEC " + index);
+		for(var i = 0; i < sockets.length; i++){
+		sockets[i].write(name + ' left the conversation');
+		}
+		console.log(name + ' left the conversation');
 	})
 
 
