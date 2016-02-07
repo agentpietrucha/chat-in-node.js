d54429246cf8a57ba8c06b8a2a4287eeae5fa21a
agentpietrucha <agentpietrucha@gmail.com>
2016-02-07 22:31:23 +0100

    organized code a little bit more

--- netClient.js
+++ netClient.js
@@ -10,7 +10,7 @@ const client = net.connect({port: 8080, host: address}, function() {
 
 client.on('error', function(error){
 	if(error.code === 'EHOSTUNREACH'){
-		console.log("Can't connect to the server. Please, check if provided IP address is correct u fuckin nigga!");	
+		console.log('Can\'t connect to the server. Please, check if provided IP address is correct u fuckin nigga!');	
 	} else if(error.code === 'ECONNREFUSED'){
 		console.log('It seems that, the server is still sleeping, after working very hard last nigh, in contrast to you, you piece of SHIT.');
 	}
@@ -21,7 +21,7 @@ client.on('data', function(data){
 });
 
 client.on('end', function(){
-	console.log("Server had ended his work, and went sleep. Please, don't wake him up");
+	console.log('Server had ended his work, and went sleep. Please, don\'t wake him up');
 	client.end();
 });
 function type(where){
