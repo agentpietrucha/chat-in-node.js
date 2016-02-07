b0bfc7edcbea22aae2e6836da40f7c0c41c28db6
agentpietrucha <agentpietrucha@gmail.com>
2016-02-08 00:18:25 +0100

    updated chatHistory. Version 1.2. History is now private variable. And cleaned code with eslint

--- netServer.js
+++ netServer.js
@@ -1,23 +1,22 @@
-var net = require('net');
+const net = require('net');
 
-var sockets = [];
-var namesList = [];
-var chatHistory = {
-	history: [],
-	add: function(data){
-		if(chatHistory.history.length === 20){
-			chatHistory.del(0, 1);
+const sockets = [];
+const namesList = [];
+
+const chatHistory = (function(){
+	var history = [];
+	return{
+		add: function(data){
+			if(history.length === 20){
+				history.splice(0, 1);
+			}
+			history.push(data);
+		},
+		get: function(){
+			return history;
 		}
-		chatHistory.history.push(data);
-	},
-	get: function(){
-		return chatHistory.history;
-	},
-	del: function(where, howMany){
-		chatHistory.history.splice(where, howMany)
-	},
-}
-
+	};
+}());
 
 var server = net.createServer(function(socket){
 	var name = '';
@@ -28,55 +27,56 @@ var server = net.createServer(function(socket){
 	console.log('number of sockets: ' + sockets.length);
 
 	function removeNewLines(str){
-		return str.replace(/(\r\n|\n|\r)/gm,"");
+		return str.replace(/(\r\n|\n|\r)/gm,'');
 	}
 	socketWrite(socket, 'gimmie you nick nigga');
 
 	socket.on('data', function(chunk){
-	var data = trimNull(removeNewLines(chunk.toString()).trim());
+		var data = trimNull(removeNewLines(chunk.toString()).trim());
 
-	if(!hasName){
-		if(namesList.indexOf(data) !== -1){
-			socketWrite(socket, 'choose another name');
-			return;
+		if(!hasName){
+			if(namesList.indexOf(data) !== -1){
+				socketWrite(socket, 'choose another name');
+				return;
+			}
+			name = data.trim();
+			writeToThisOne(socketWrite(socket, 'active users: ' + namesList));
+			var history = chatHistory.get();
+			for(var i = 0; i < history.length; i++){
+				// writeToThisOne(socketWrite(socket, chatHistory[i]));
+				writeToThisOne(socketWrite(socket, history[i]));
+			}
+			console.log(history);
+			socketWrite(socket, 'Hi, ' + name);
+			namesList.push(name);
+			hasName = true;
 		}
-		name = data.trim();
-		writeToThisOne(socketWrite(socket, 'active users: ' + namesList));
-		for(var i = 0; i < chatHistory.history.length; i++){
-			// writeToThisOne(socketWrite(socket, chatHistory[i]));
-			writeToThisOne(socketWrite(socket, chatHistory.get()[i]));
-		}
-		console.log(chatHistory.history);
-		socketWrite(socket, 'Hi, ' + name);
-		namesList.push(name);
-		hasName = true;
-	}
 
-	console.log('client:' + name + ' ' + data);
-	var message = name + ': ' + data;
-	if(numMessagesFromClient === 0){
-		message = name + ' ' + 'joined';
-		// chatHistory.push(name + ' joined');
-		chatHistory.add(name + ' joined');
-	} else{
+		console.log('client:' + name + ' ' + data);
+		var message = name + ': ' + data;
+		if(numMessagesFromClient === 0){
+			message = name + ' ' + 'joined';
+			// chatHistory.push(name + ' joined');
+			chatHistory.add(name + ' joined');
+		} else{
+			// chatHistory.push(name + ': ' + data);
+			chatHistory.add(name + ': ' + data);
+		}
+		// if(chatHistory.length === 21){
+		// 	chatHistory.splice(0, 1);
+		// }
+		// chatHistory.check();
+
+		writeAll(message, socket);
+		numMessagesFromClient += 1;
 		// chatHistory.push(name + ': ' + data);
-		chatHistory.add(name + ': ' + data);
-	}
-	// if(chatHistory.length === 21){
-	// 	chatHistory.splice(0, 1);
-	// }
-	// chatHistory.check();
 
-	writeAll(message, socket);
-	numMessagesFromClient += 1;
-	// chatHistory.push(name + ': ' + data);
-
-	})
+	});
 	socket.on('end', function(){
 		sockets.splice(sockets.indexOf(socket), 1);
 		namesList.splice(namesList.indexOf(name), 1);
 		for(var i = 0; i < sockets.length; i++){
-		socketWrite(sockets[i], name + ' left the conversation')
+			socketWrite(sockets[i], name + ' left the conversation');
 		}
 		console.log(name + ' left the conversation');
 	});
@@ -95,12 +95,11 @@ function writeAll (message, socket) {
 }
 
 function trimNull(what) {
-  	var index = what.indexOf('\0');
+	var index = what.indexOf('\0');
 	if (index > -1) {
-    	return what.substr(0, index);
-  	} else{
-  		return what;
-  	}
+		return what.substr(0, index);
+	}
+	return what;
 }
 
 function writeToThisOne(thisone, ms){
