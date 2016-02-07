20bfd623a0b62b31da95bc3d5386880a68f03136
agentpietrucha <agentpietrucha@gmail.com>
2016-02-07 22:29:06 +0100

    straighten up the code and added function which checks if argument provided by user is valid. If its not it is localhost

--- netClient.js
+++ netClient.js
@@ -1,7 +1,8 @@
 const net = require('net');
 var address = process.argv[2];
-var number = 0;
-var name = '';
+if(process.argv[2] === undefined){
+	address = 'localhost';
+}
 
 const client = net.connect({port: 8080, host: address}, function() {
 	type(client);
@@ -9,20 +10,20 @@ const client = net.connect({port: 8080, host: address}, function() {
 
 client.on('error', function(error){
 	if(error.code === 'EHOSTUNREACH'){
-			console.log("Can't connect to the server. Please, check if provided IP address is correct u fuckin nigga!");	
+		console.log("Can't connect to the server. Please, check if provided IP address is correct u fuckin nigga!");	
 	} else if(error.code === 'ECONNREFUSED'){
-		console.log("It seems that, the server is still sleeping, after working very hard last nigh, in contrast to you, you piece of SHIT.")
+		console.log('It seems that, the server is still sleeping, after working very hard last nigh, in contrast to you, you piece of SHIT.');
 	}
 	console.log(error);
-})
+});
 client.on('data', function(data){
 	process.stdout.write( data.toString());
-})
+});
 
 client.on('end', function(){
 	console.log("Server had ended his work, and went sleep. Please, don't wake him up");
 	client.end();
-})
+});
 function type(where){
 	var stdin = process.openStdin();
 	stdin.addListener('data', function(data){
