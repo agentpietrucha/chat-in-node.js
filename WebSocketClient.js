f92ac889c243720ec7a5420ef13c1e6322ad1030
agentpietrucha <agentpietrucha@gmail.com>
2016-03-11 23:05:13 +0100

    working state on localstorage

--- WebSocketClient.js
+++ WebSocketClient.js
@@ -10,35 +10,49 @@ var name = '';
 // var toWhoInput = document.querySelector('#toWhoInput');
 var activeUsers = document.querySelector('.activeUsers');
 var user = document.querySelector('.user');
+var chattingUsers = document.querySelector('.chattingUsers');
 
-activeUsers.addEventListener('click', function(e){
-	console.log(e.target.innerText);
-	recipient = e.target.innerText;
-	sendJSON('toWho', recipient, null);
-	user.innerHTML = recipient;
-	document.querySelector('.inputContainer').style.display = 'inline-block';
-});
-
+function updateScroll(){
+	textArea.scrollTop = textArea.scrollHeight;
+}
 function sendNick() {
 	name = nicknameInput.value;
 	sendJSON('nickname', name);
 	console.log('NAME' + nicknameInput.value);
 	nicknameInput.value = '';
-	document.querySelector('.toWhoContainer').style.display = 'block';
+	nicknameInput.placeholder = 'Hi, ' + name;
 }
 function sendMessage(){
 	sendJSON('message', recipient, textInput.value);
 	createAndInsertElement('p', 'right', textInput.value, textArea);
+	textArea.scrollTop = textArea.scrollHeight;
+	nameMessageMapping.add(name, textInput.value, recipient, 'outcoming');
 	textInput.value = '';
 }
-// function sendToWho(){
-// 	console.log("setting recipient", toWhoInput.value);
-// 	recipient = toWhoInput.value;
-// 	sendJSON('toWho', recipient, null);
-// 	toWhoInput.value = '';
-// 	user.innerHTML = toWhoInput.value;
-// 	document.querySelector('.inputContainer').style.display = 'block';
-// }
+var recipients = [];
+var costam = true;
+activeUsers.addEventListener('click', function(e){
+	recipient = e.target.innerText;
+	recipients.push(recipient);
+	while(textArea.firstChild){
+		textArea.removeChild(textArea.firstChild);
+	}
+	sendJSON('toWho', recipient, null);
+	if(recipient !== name){
+		// while(costam){
+		user.innerHTML = recipient;
+		// for(var i = 0; i < recipients.length; i++){
+		// 	if(recipients[i] === document.querySelectorAll('.chattingUsers p')){
+		// 		return;
+		// 	} else{
+		 		createAndInsertElement('p', null, recipient, chattingUsers);	
+		// 	}
+		// }
+			// costam = false;
+		// }
+	}
+	document.querySelector('.inputContainer').style.display = 'inline-block';
+});
 nickButton.addEventListener('click', function(){
 	// connection.send(nicknameInput.value);
 	sendNick();
@@ -72,10 +86,9 @@ function sendJSON(type, name, message){
 	connection.send(JSON.stringify(out));
 }
 connection.onmessage = function(e){
+	updateScroll();
 	var data = JSON.parse(e.data);
-	console.log('message form server: ' + e.data);
 	if(data.type === 'activeUsersList'){
-		console.log('activeUsers: ', data.message);
 		while(activeUsers.firstChild){
 			activeUsers.removeChild(activeUsers.firstChild);
 		}
@@ -87,10 +100,52 @@ connection.onmessage = function(e){
 		createAndInsertElement('h1', null, data.message, body);
 	}
 	if(data.type === 'message'){
+		recipient = data.fromWho;
+		nameMessageMapping.add(recipient, data.message, recipient, 'income');
+		// nameMessageMapping.setInStorage(recipient);
+		console.log('LOCALSTORAGE', localStorage.getItem('messages'));
 		console.log('Message from user: ' + data.message);
+		sendJSON('toWho', recipient, null);
 		createAndInsertElement('p', 'left', data.message, textArea);
+		while(costam){
+			user.innerHTML = recipient;
+			createAndInsertElement('p', null, recipient, chattingUsers);
+			costam = false;
+		}
 	}
 }
+connection.onerror = function(error){
+	console.log('ERROR', error);
+}
+var nameMessageMapping = (function(){
+	var mapping = {};
+	return{
+		add: function(name, message, recipient, type){
+			if(mapping[name] === undefined){
+				console.log('first if in mapping');
+				mapping[name] = new Object;
+				mapping[name].type = type;
+				mapping[name].recipient = recipient;
+				mapping[name].message = new Array;
+				mapping[name].message.push(message);
+			} else{
+				console.log('second if in mapping');
+				mapping[name].message.push(message);
+			}
+			console.log(mapping);
+		},
+		get: function(name){
+			return localStorage.getItem('name');
+		},
+		setInStorage: function(name){
+			localStorage.clear();
+			localStorage.setItem(name + recipient, mapping[name]);
+		},
+		getStorage: function(){
+			localStorage.getItem(name + recipient);
+		}
+	}
+}());
 function createAndInsertElement(element, clss, message, where){
 	var element = document.createElement(element);
 	element.classList.add(clss);
